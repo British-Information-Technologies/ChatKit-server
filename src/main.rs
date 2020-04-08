f34b2c5cd728b371b3e3f8c8ffff0521dd425652
Mitchell <mitchellhardie1@gmail.com>
2020-04-08 23:15:06 +0000

    added basic connection handler
    Every connection the server recieves a new handle function is called

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
 use std::net::TcpListener;
+use std::net::TcpStream;
+use std::io::prelude::*;
 
 fn main(){
     let listener = TcpListener::bind("127.0.0.1:6001").unwrap();
@@ -6,6 +8,12 @@ fn main(){
     for stream in listener.incoming(){
         let stream = stream.unwrap();
 
-        println!("Connection Established!");
+        handle_connection(stream);
     }
+}
+
+fn handle_connection(mut stream: TcpStream){
+    let mut buffer = [0; 512];
+    stream.read(&mut buffer).unwrap();
+    println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
 }
