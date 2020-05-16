2e1d4d3b401e9cb6d132a509ce1200833f782f0e
Mitchell <mitchellhardie1@gmail.com>
2020-05-16 18:07:09 +0000

    temp fix for struct values

--- src/client_management/client_profile.rs
+++ src/client_management/client_profile.rs
@@ -1,19 +1,19 @@
 pub struct Client{
-    uuid: String,
-    username: String,
-    address: String,
+    pub uuid: String,
+    pub username: String,
+    pub address: String,
 }
 
 impl Client{
-    pub fn get_uuid(&self) -> String{
-        self.uuid
+    pub fn get_uuid(&self) -> &String{
+        &self.uuid
     }
 
-    pub fn get_username(&self) -> String{
-        self.username
+    pub fn get_username(&self) -> &String{
+        &self.username
     }
 
-    pub fn get_addres(&self) -> String{
-        self.address
+    pub fn get_addres(&self) -> &String{
+        &self.address
     }
 }
