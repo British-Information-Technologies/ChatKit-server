7ed9e469fcc28d1eb484842bb54d8f45de092c11
Mitch161 <mitchellhardie@live.co.uk>
2020-07-19 17:55:10 +0100

    changed String to 'static str
    The server details shouldnt be able to be change and should exist for the whole execution of the program, this can be changed by reverting back to the old format

--- src/main.rs
+++ src/main.rs
@@ -7,12 +7,19 @@ use std::net::{TcpStream, TcpListener};
 use rust_chat_server::ThreadPool;
 use std::sync::{Arc, Barrier, Mutex};
 use std::collections::HashMap;
+use crate::server::commands::Commands;
+use crossbeam_channel::Sender;
 
 fn main(){
+    let server_name: &'static str = "Server-01";
+    let server_address: &'static str = "0.0.0.0:6000";
+    let server_author: &'static str = "noreply@email.com";
+    /*
     let server_name = String::from("Server-01");
     let server_address = String::from("0.0.0.0:6000");
     let server_owner = String::from("noreply@email.com");
+    */
 
-    let server = Server::new(&server_name, &server_address, &server_owner);
-    server.start();
+    let server = Server::new(server_name, server_address, server_author);
+    //server.start();
 }
