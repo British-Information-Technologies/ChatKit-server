ea98cc7688528e8c3ed5d17d17c803e0c43e11b7
michael-bailey <mickyb18a@gmail.com>
2020-08-16 17:13:28 +0100

    removed old commands files.

--- src/server/commands/client_update.rs
+++
@@ -1,5 +0,0 @@
-/*use crate::server::client::client_profile::Client;
-
-pub fn format_client_data(uuid: &String, client: &Client) -> String{
-    ["!client: username:",client.get_username(), " uuid:", uuid, " host:\"", client.get_address(), "\""].concat()
-}*/
