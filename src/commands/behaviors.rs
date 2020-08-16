f24a4f72dff65b8b0a258497f3019bbae269e373
Mitchell <mitchellhardie1@gmail.com>
2020-08-16 17:15:23 +0100

    functionality for individual commands

---
+++ src/commands/behaviors.rs
@@ -0,0 +1,73 @@
+struct Request {}
+
+struct Info {}
+
+struct Connect {}
+
+struct Disconnect {}
+
+struct ClientUpdate {}
+
+struct ClientInfo {}
+
+struct ClientRemove {}
+
+struct Client {}
+
+struct Success {}
+
+struct Error {}
+
+trait Runnables {
+    fn run(&self);
+}
+
+impl Runnables for Request {
+    fn run() {
+    }
+}
+
+impl Runnables for Request {
+    fn run() {
+    }
+}
+
+impl Runnables for Request {
+    fn run() {
+    }
+}
+
+impl Runnables for Request {
+    fn run() {
+    }
+}
+
+impl Runnables for Request {
+    fn run() {
+    }
+}
+
+impl Runnables for Request {
+    fn run() {
+    }
+}
+
+impl Runnables for Request {
+    fn run() {
+    }
+}
+
+impl Runnables for Request {
+    fn run() {
+    }
+}
+
+impl Runnables for Request {
+    fn run() {
+    }
+}
+
+impl Runnables for Request {
+    fn run() {
+    }
+}
