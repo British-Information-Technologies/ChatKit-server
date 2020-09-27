b0ed33b5c732308a581483b5e9a0fe689a643ad6
michael-bailey <mickyb18a@gmail.com>
2020-09-27 16:58:24 +0100

    Update main_menu.rs
    fixed accidental renaming

--- src/server/ui/main_menu.rs
+++ src/server/ui/main_menu.rs
@@ -4,7 +4,7 @@ use crate::server::ui::about_panel::about;
 
 pub fn main_menu() -> MenuTree {
     MenuTree::new()
-        .leaf("About ^+A", |s| s.add_layer(About::new()))
+        .leaf("About ^+A", |s| s.add_layer(about()))
         .delimiter()
         .leaf("Quit  ^+Q", |s| s.quit())
 }
