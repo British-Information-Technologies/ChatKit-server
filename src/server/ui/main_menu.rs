5b60f9282d96a07fad5797333a337872d67aa6b2
michael-bailey <mickyb18a@gmail.com>
2020-09-27 16:56:29 +0100

    Update main_menu.rs
    renamed the main menu function

--- src/server/ui/main_menu.rs
+++ src/server/ui/main_menu.rs
@@ -1,6 +1,8 @@
 use cursive::menu::MenuTree;
 
-pub fn main_Menu() -> MenuTree {
+use crate::server::ui::about_panel::about;
+
+pub fn main_menu() -> MenuTree {
     MenuTree::new()
         .leaf("About ^+A", |s| s.add_layer(About::new()))
         .delimiter()
