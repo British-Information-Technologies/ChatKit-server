5243a6ce8e6c2778ff5abefd4483700eff5076b0
michael-bailey <mickyb18a@gmail.com>
2020-09-27 16:07:45 +0100

    renamed the about panel.

--- src/server/ui/about_panel.rs
+++ src/server/ui/about_panel.rs
@@ -2,10 +2,10 @@ use cursive::views::{Dialog, TextView};
 use cursive::view::ViewWrapper;
 use cursive::{Printer, View};
 
-pub fn About() -> Box<dyn View> {
+pub fn about() -> Box<dyn View> {
     Box::new(
         Dialog::new()
-            .content("rust chat server written by Mitchel Hardie & Michael Bailey (c) 2020")
+            .content(TextView::new("rust chat server written by Mitchel Hardie & Michael Bailey (c) 2020"))
             .button("Close", |s| {s.pop_layer();})
     )
 }
