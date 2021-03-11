3591318270f28675b18b61771e4b4d212673da16
michael-bailey <mickyb18a@gmail.com>
2021-03-11 14:06:05 +0000

    Fixed immediate warnings

--- src/lib/server/client_management/traits.rs
+++ src/lib/server/client_management/traits.rs
@@ -6,8 +6,8 @@ use uuid::Uuid;
  * @michael-bailey
  */
 pub trait TClientManager<TClient,TClientMessage> {
-  fn addClient(&self, client: Arc<TClient>);
-  fn removeClient(&self, id: Uuid);
-  fn messageClient(&self, id: Uuid, msg: TClientMessage);
+  fn add_client(&self, client: Arc<TClient>);
+  fn remove_client(&self, id: Uuid);
+  fn message_client(&self, id: Uuid, msg: TClientMessage);
   fn tick(&self, );
 }
