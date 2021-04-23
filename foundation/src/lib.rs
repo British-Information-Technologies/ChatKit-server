137ef3d3b1f0ceb2d78a567dc1991d943779f066
michael-bailey <mickyb18a@gmail.com>
2021-04-23 15:14:45 +0000

    Adding public key storage

--- foundation/src/lib.rs
+++ foundation/src/lib.rs
@@ -3,10 +3,12 @@ pub mod prelude;
 
 use serde::{Deserialize, Serialize};
 use uuid::Uuid;
+// use ring::signature::RsaPublicKeyComponents;
 
 #[derive(Deserialize, Serialize, Debug, Clone)]
 pub struct ClientDetails {
 	pub uuid: Uuid,
 	pub username: String,
 	pub address: String,
+	// pub public_key: Option<RsaPublicKeyComponents<>>,
 }
