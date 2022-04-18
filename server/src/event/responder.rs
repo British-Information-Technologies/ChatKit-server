403ba73bbb1e67341958ebaac10c7ac5af070072
michael-bailey <mickyb18a@gmail.com>
2022-04-18 13:00:27 +0100

    renamed responder module

--- server/src/event/responder.rs
+++ server/src/event/responder.rs
@@ -1,6 +1,6 @@
 use crate::event::Event;
 
 pub(crate) trait IResponder {
-	fn accepts_event(&self, event: Event) -> bool;
+	fn accepts_event(&self, event: &Event) -> bool;
 	fn on_event(&self, event: Event);
 }
