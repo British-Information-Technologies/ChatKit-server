04aef9cd9f7a8d5ccd8400e0cc4f5acae602cfee
michael-bailey <mickyb18a@gmail.com>
2022-04-21 08:20:22 +0100

    created a event result builder

--- server/src/event/event_result.rs
+++ server/src/event/event_result.rs
@@ -1,3 +1,4 @@
+use futures::channel::oneshot::Sender;
 use std::collections::HashMap;
 
 pub enum EventResultType {
@@ -10,4 +11,45 @@ pub enum EventResultType {
 pub struct EventResult {
 	code: EventResultType,
 	args: HashMap<String, String>,
+}
+
+impl EventResult {
+	pub fn create(result_type: EventResultType) -> EventResultBuilder {
+		EventResultBuilder::new(result_type)
+	}
+}
+
+pub struct EventResultBuilder {
+	code: EventResultType,
+	args: HashMap<String, String>,
+}
+
+impl EventResultBuilder {
+	pub(self) fn new(result_type: EventResultType) -> Self {
+		Self {
+			code: result_type,
+			args: HashMap::default(),
+		}
+	}
+
+	pub fn add_arg(mut self, key: String, value: String) -> Self {
+		self.args.insert(key, value);
+		self
+	}
+
+	pub fn build(self) -> EventResult {
+		EventResult {
+			code: self.code,
+			args: self.args,
+		}
+	}
+
+	pub fn send(self, sender: Sender<EventResult>) {
+		sender
+			.send(EventResult {
+				code: self.code,
+				args: self.args,
+			})
+			.ok();
+	}
 }
