9f88a81c2ad354a4a0c7ba9947263e262cdec0c0
michael-bailey <mickyb18a@gmail.com>
2022-04-23 17:14:27 +0100

    added functions for IResponder

--- foundation/src/event/responder.rs
+++ foundation/src/event/responder.rs
@@ -1,6 +1,17 @@
 use crate::event::Event;
+use std::sync::Weak;
 
-pub(crate) trait IResponder {
-	fn accepts_event(&self, event: &Event) -> bool;
+pub trait IResponder {
+	fn post_event(&self, event: Event) {
+		if let Some(next) = self.get_next() {
+			if let Some(next) = next.upgrade() {
+				next.post_event(event);
+				return;
+			}
+		}
+		self.r#final(event);
+	}
+	fn get_next(&self) -> Option<Weak<dyn IResponder>>;
 	fn on_event(&self, event: Event);
+	fn r#final(&self, _event: Event) {}
 }
