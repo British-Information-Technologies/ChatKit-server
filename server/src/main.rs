58df6bc7a8b21e9bf2806b28b866d67addb38d43
michael-bailey <mickyb18a@gmail.com>
2022-06-15 18:32:04 +0200

    renamed old files

--- server/src/main.rs
+++ server/src/main.rs
@@ -3,10 +3,10 @@
 //! It starts the actor runtime and then sleeps
 //! for the duration of the program.
 
+pub(crate) mod server;
 pub(crate) mod client_management;
 pub(crate) mod network;
 pub(crate) mod prelude;
-pub(crate) mod server;
 
 use server::ServerActor;
 use tokio::time::{sleep, Duration};
