bb8d8aa3b4a62e7ce5d58ec85a3cc48ad8f18ab1
michael-bailey <mickyb18a@gmail.com>
2022-09-15 08:09:58 +0100

    cleaned up prelude linting errors

--- server/src/prelude/mod.rs
+++ server/src/prelude/mod.rs
@@ -3,17 +3,25 @@
 
 mod observer;
 
+#[allow(unused_imports)]
 pub mod actors {
 	//! exports all actors used in the program.
-	pub use crate::server::Server;
-	pub(crate) use crate::network::{Connection, ConnectionInitiator, NetworkManager};
-	pub(crate) use crate::client_management::ClientManager;
 	pub(crate) use crate::client_management::client::Client;
+	pub(crate) use crate::client_management::ClientManager;
+	pub(crate) use crate::network::{
+		Connection, ConnectionInitiator, NetworkManager,
+	};
+	pub use crate::server::Server;
 }
+
+#[allow(unused_imports)]
 pub mod messages {
 	//! exports all messages used in the program.
 	pub(crate) use super::observer::ObservableMessage;
-	pub(crate) use crate::network::{ConnectionMessage, ConnectionOuput, NetworkMessage, NetworkOutput};
-	pub(crate) use crate::client_management::{ClientManagerMessage, ClientManagerOutput};
-
+	pub(crate) use crate::client_management::{
+		ClientManagerMessage, ClientManagerOutput,
+	};
+	pub(crate) use crate::network::{
+		ConnectionMessage, ConnectionOuput, NetworkMessage, NetworkOutput,
+	};
 }
