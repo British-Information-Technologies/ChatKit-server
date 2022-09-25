d9a67191624a1820c87e38e3aa10ba7442e33244
michael-bailey <mickyb18a@gmail.com>
2022-09-25 12:26:36 +0100

    refactord network manager module name

--- server/src/network/network_manager/mod.rs
+++ server/src/network/network_manager/mod.rs
@@ -2,12 +2,15 @@
 //! This module contains the network manager actor
 //! it's role involves handling new oncomming network connections
 
+mod actor;
 mod builder;
 mod messages;
-mod network_manager;
 
+pub(crate) use actor::NetworkManager;
 pub(crate) use builder::*;
 pub(crate) use messages::{
-	NetworkDataMessage, NetworkDataOutput, NetworkMessage, NetworkOutput,
+	NetworkDataMessage,
+	NetworkDataOutput,
+	NetworkMessage,
+	NetworkOutput,
 };
-pub(crate) use network_manager::NetworkManager;
